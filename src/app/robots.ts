f5e6770efaa8cf64306fede97530734be058b02d
Cody West <cww2697@gmail.com>
2025-12-21 19:29:17 -0600

    Update robots.txt rules to refine bot access and improve SEO configuration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,10 +4,25 @@ export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: 'ai-train',
+        disallow: '/',
+      },
+      {
+        userAgent: 'ai-input',
+        allow: ['/projects/spark-launcher', '/projects/terravista'],
+        disallow: '/',
+      },
+      {
+        userAgent: ['GPTBot', 'ChatGPT-User', 'Google-Extended', 'CCBot', 'Anthropic-ai', 'Claude-Web', 'ClaudeBot', 'PerplexityBot', 'Amazonbot', 'ImagesiftBot'],
+        disallow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://www.canyonwolf.net/sitemap.xml',
   }
 }
